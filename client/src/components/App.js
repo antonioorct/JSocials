140d82af5f27179458c988ff72bdcd80a6052585
Antet <anteexpress@hotmail.com>
2020-12-22 09:42:05 +0100

    Add header bar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component, Fragment } from "react";
 import { Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
 
+import HeaderBar from "./navbar";
 import Register from "./register";
 import Login from "./login";
 import Main from "./main";
@@ -10,14 +12,16 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <header></header>
-        <main>
-          <Switch>
-            <Route path="/register" component={Register}></Route>
-            <Route path="/login" component={Login}></Route>
-            <Route path="/" component={Main}></Route>
-          </Switch>
-        </main>
+        <HeaderBar />
+        <Container>
+          <main>
+            <Switch>
+              <Route path="/register" component={Register}></Route>
+              <Route path="/login" component={Login}></Route>
+              <Route path="/" component={Main}></Route>
+            </Switch>
+          </main>
+        </Container>
       </Fragment>
     );
   }
