4b8d21f98d3edac3704a01e569cde39f567cdc54
Antet <anteexpress@hotmail.com>
2020-12-26 22:08:59 +0100

    rename login function

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,5 +1,4 @@
 import http from "./httpService";
-
 import jwtDecode from "jwt-decode";
 
 const apiEndPoint = "http://localhost:3001/api/";
@@ -8,11 +7,12 @@ export async function register(email, password) {
   await http.post(apiEndPoint + "users", { email, password });
 }
 
-export async function login(email, password) {
+export async function sendLoginInfo(email, password) {
   const { data: jwt } = await http.post(apiEndPoint + "auth", {
     email,
     password,
   });
+
   return jwt;
 }
 
