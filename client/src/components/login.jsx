c9e2d9f0996b1296ef6190fbecb2a8f6318ece4a
Antet <anteexpress@hotmail.com>
2020-12-26 22:09:37 +0100

    login form updates

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,49 +1,53 @@
-import React, { Component } from "react";
-import { login } from "../services/authService";
+import React, { Component, useContext, useState } from "react";
+import { sendLoginInfo } from "../services/authService";
 
-class Login extends Component {
-  state = { email: "", password: "" };
+import { UserContext } from "../contexts/UserContext";
 
-  handleChange = (e) => {
-    this.setState({ [e.target.id]: e.target.value });
+function Login() {
+  const [userForm, setUserForm] = useState({ email: "", password: "" });
+  const [user, setUser] = useContext(UserContext);
+
+  const handleChange = (e) => {
+    setUserForm({ ...userForm, [e.target.id]: e.target.value });
   };
 
-  handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
     try {
-      const jwt = await login(this.state.email, this.state.password);
+      const jwt = await sendLoginInfo(this.state.email, this.state.password);
       localStorage.setItem("token", jwt);
+
       this.props.history.push("/");
+      this.props.user = { email: "test" };
     } catch (e) {
       console.log(e);
     }
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          E-Mail
-          <input
-            type="text"
-            id="email"
-            value={this.state.email}
-            onChange={this.handleChange}
-          ></input>
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            id="password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          ></input>
-        </label>
-        <input className="btn btn-primary" type="submit" value="Login"></input>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        E-Mail
+        <input
+          type="text"
+          id="email"
+          value={userForm.email}
+          onChange={handleChange}
+        ></input>
+      </label>
+      <label>
+        Password
+        <input
+          type="password"
+          id="password"
+          value={userForm.password}
+          onChange={handleChange}
+        ></input>
+      </label>
+      <input className="btn btn-primary" type="submit" value="Login"></input>
+    </form>
+  );
 }
 
 export default Login;
