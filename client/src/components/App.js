de9c7fef5a46aa895f7394ee893b0a4e82b211e6
Antet <anteexpress@hotmail.com>
2020-12-27 20:51:54 +0100

    add ProtectedRoute and protected routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,40 +1,46 @@
 import React, { Component, useEffect, useState } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import { UserContext, initialState } from "../contexts/UserContext";
 import { getLoggedInUser } from "../services/authService";
 
+import ProtectedRoute from "../components/protectedRoute";
 import HeaderBar from "./headerBar";
 import Register from "./register";
 import Login from "./login";
 import Main from "./main";
 
 export default function App() {
-  const [user, setUser] = useState(initialState);
+  const [user, setUser] = useState(null);
+
+  const fetchAndSetUser = async () => {
+    const loggedInUser = await getLoggedInUser();
+
+    setUser(loggedInUser);
+  };
 
   useEffect(() => {
-    async function fetchUser() {
-      const loggedInUser = await getLoggedInUser();
-
-      setUser(loggedInUser);
-    }
-    fetchUser();
+    fetchAndSetUser();
   }, []);
 
-  return (
+  return user ? (
     <UserContext.Provider value={[user, setUser]}>
       <HeaderBar />
       <Container>
         <Switch>
-          <Route path="/register" component={Register} exact></Route>
           <Route path="/login" component={Login} exact></Route>
-          <Route path="/" component={Main} exact></Route>
+          <Route path="/register" component={Register} exact></Route>
+
+          <ProtectedRoute path="/" component={Main} exact></ProtectedRoute>
+
           <Route path="*">
             <div>404 Not Found</div>
           </Route>
         </Switch>
       </Container>
     </UserContext.Provider>
+  ) : (
+    <div />
   );
 }
