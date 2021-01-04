8dd0feb0b6d926540ac54867ab1776128c15cd91
Antet <anteexpress@hotmail.com>
2021-01-04 14:34:42 +0100

    disable sequelize logging

--- server/sequelize.js
+++ server/sequelize.js
@@ -1,19 +1,17 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:3306/${process.env.DB_DB}`
+  `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:3306/${process.env.DB_DB}`,
+  { logging: false }
 );
 
-// try {
-//    sequelize.authenticate();
-//   console.log("Database connection OK!");
-// } catch (error) {
-//   console.log("Unable to connect to the database:");
-//   console.log(error.message);
-//   process.exit(1);
-// }
-
-console.log("Connected to database");
+try {
+  console.log("Connected to database.");
+} catch (error) {
+  console.log("Unable to connect to the database:");
+  console.log(error.message);
+  process.exit(1);
+}
 
 const models = [
   require("./models/user.model"),
