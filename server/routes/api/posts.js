74f4fcf6ed59f147345a8a811c8d8d795838a20c
Antet <anteexpress@hotmail.com>
2021-01-24 19:31:55 +0100

    rewrite posts API route

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -3,30 +3,53 @@ const express = require("express");
 const { Op } = require("sequelize");
 const router = express.Router();
 const { models } = require("../../sequelize");
+const { userExists, postExists } = require("../../middleware/userCheck");
+const { authenticate, authenticateSameUser } = require("../../middleware/jwt");
 const fs = require("fs");
 const multer = require("multer")();
 
-router.get("", async function (req, res) {
-  const userId = req.query.userId;
+router.get(
+  "/users/:userId/feed",
+  authenticateSameUser,
+  async function (req, res, next) {
+    try {
+      const userId = req.userId;
 
-  const friends = await models.user.findAll({
-    include: [{ model: models.user, as: "friends", attributes: ["id"] }],
-    where: { id: userId },
-  });
+      const friends = await models.user.findAll({
+        include: [{ model: models.user, as: "friends", attributes: ["id"] }],
+        where: { id: userId },
+      });
+      if (!friends) return res.status(404).send();
 
-  const friendIds = friends[0]
-    .getDataValue("friends")
-    .map((friend) => friend.getDataValue("id"));
+      const friendIds = friends[0]
+        .getDataValue("friends")
+        .map((friend) => friend.getDataValue("id"));
 
-  const posts = await models.post.findAll({
-    include: [
-      {
-        model: models.post,
-        as: "comments",
+      const posts = await models.post.findAll({
         include: [
           {
+            model: models.post,
+            as: "comments",
+            include: [
+              {
+                model: models.user,
+                attributes: ["firstName", "lastName", "username", "id"],
+              },
+              {
+                model: models.userPostLike,
+                include: {
+                  model: models.user,
+                  attributes: ["firstName", "lastName"],
+                },
+              },
+            ],
+            limit: 3,
+            attributes: { exclude: ["user_id"] },
+            order: [["createdAt", "DESC"]],
+          },
+          {
             model: models.user,
-            attributes: ["firstName", "lastName", "username", "id"],
+            attributes: ["firstName", "lastName", "username"],
           },
           {
             model: models.userPostLike,
@@ -36,139 +59,59 @@ router.get("", async function (req, res) {
             },
           },
         ],
-        limit: 3,
-        attributes: { exclude: ["user_id"] },
-        order: [["createdAt", "DESC"]],
-      },
-      {
-        model: models.user,
-        attributes: ["firstName", "lastName", "username"],
-      },
-      {
-        model: models.userPostLike,
-        include: { model: models.user, attributes: ["firstName", "lastName"] },
-      },
-    ],
-    where: {
-      postId: { [Op.eq]: null },
-      userId: { [Op.in]: [...friendIds, userId] },
-    },
-    order: [["createdAt", "DESC"]],
-  });
-
-  if (!posts) return res.sendStatus(404);
-  else return res.status(200).send(posts);
-});
-
-router.get("/:postId", async function (req, res) {
-  const post = await models.post.findByPk(req.params.postId, {
-    include: [
-      {
-        model: models.post,
-        as: "comments",
-        include: [
-          {
-            model: models.user,
-            attributes: ["firstName", "lastName", "username", "id"],
-          },
-          {
-            model: models.userPostLike,
-            include: {
-              model: models.user,
-              attributes: ["firstName", "lastName"],
+        where: {
+          [Op.and]: {
+            postId: { [Op.eq]: null },
+            [Op.or]: {
+              [Op.and]: { private: false, userId: friendIds },
+              userId: userId,
             },
           },
-        ],
-        attributes: { exclude: ["user_id"] },
-      },
-      {
-        model: models.user,
-        attributes: ["firstName", "lastName", "username"],
-      },
-      {
-        model: models.userPostLike,
-        include: { model: models.user, attributes: ["firstName", "lastName"] },
-      },
-    ],
-    order: [[Sequelize.col("comments.created_at"), "DESC"]],
-  });
-
-  res.status(200).send(post);
-});
-
-router.get("/:postId/comments", async function (req, res) {
-  const createdAt = req.query.createdAt;
-
-  const comments = await models.post.findAll({
-    include: [
-      {
-        model: models.userPostLike,
-        include: {
-          model: models.user,
-          attributes: ["firstName", "lastName"],
         },
-      },
-      { model: models.user, attributes: ["firstName", "lastName", "id"] },
-    ],
-    where: {
-      createdAt: { [Op.lt]: createdAt },
-      postId: { [Op.eq]: req.params.postId, [Op.ne]: null },
-    },
-    order: [["createdAt", "DESC"]],
-    limit: parseInt(req.query.limit) || null,
-  });
+        order: [["createdAt", "DESC"]],
+      });
 
-  if (!comments) return res.sendStatus(404);
-  else return res.status(200).send(comments);
-});
-
-router.get("/images/:userId", async function (req, res) {
-  const images = await models.post.findAll({
-    attributes: ["id", "imagePath"],
-    order: [["createdAt", "DESC"]],
-    where: { userId: req.params.userId, imagePath: { [Op.ne]: null } },
-  });
-
-  res.status(200).send(images);
-});
-
-router.post("/", multer.single("image"), async function (req, res) {
-  try {
-    if (req.file)
-      fs.writeFile(
-        "C:\\College\\Zavrsni\\Projekt\\client\\public\\img\\" +
-          req.file.originalname,
-        req.file.buffer,
-        (e) => console.log(e)
-      );
-
-    const newPost = await models.post.create({
-      ...req.body,
-      imagePath: req.file ? req.file.originalname : null,
-    });
-    const newFullPost = await models.post.findByPk(newPost.getDataValue("id"), {
-      include: [{ model: models.user, attributes: ["firstName", "lastName"] }],
-      attributes: { exclude: ["postId"] },
-    });
-    newFullPost.setDataValue("comments", []);
-    newFullPost.setDataValue("userPostLikes", []);
-
-    res.status(201).send(newFullPost);
-  } catch (e) {
-    res.status(400).send("Error creating user:\n" + e.message);
+      if (!posts) return res.sendStatus(404);
+      return res.status(200).send(posts);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
-router.post("/:postId", async function (req, res) {
-  try {
-    const newComment = await models.post.create(req.body);
-    await models.post.increment("numComments", {
-      where: { id: newComment.getDataValue("postId") },
-    });
-    const newFullComment = await models.post.findByPk(
-      newComment.getDataValue("id"),
-      {
+router.get(
+  "/users/:userId/posts",
+  [authenticate, userExists],
+  async function (req, res, next) {
+    try {
+      const userId = req.params.userId;
+
+      const posts = await models.post.findAll({
         include: [
+          {
+            model: models.post,
+            as: "comments",
+            include: [
+              {
+                model: models.user,
+                attributes: ["firstName", "lastName", "username", "id"],
+              },
+              {
+                model: models.userPostLike,
+                include: {
+                  model: models.user,
+                  attributes: ["firstName", "lastName"],
+                },
+              },
+            ],
+            limit: 3,
+            attributes: { exclude: ["user_id"] },
+            order: [["createdAt", "DESC"]],
+          },
+          {
+            model: models.user,
+            attributes: ["firstName", "lastName", "username"],
+          },
           {
             model: models.userPostLike,
             include: {
@@ -176,46 +119,155 @@ router.post("/:postId", async function (req, res) {
               attributes: ["firstName", "lastName"],
             },
           },
-          { model: models.user },
         ],
-      }
-    );
+        where: {
+          postId: { [Op.eq]: null },
+          userId: userId,
+          private: userId === req.userId ? null : false,
+        },
+        order: [["createdAt", "DESC"]],
+      });
 
-    res.status(201).send(newFullComment);
-  } catch (e) {
-    console.log(e);
-    res.status(400).send("Error creating user:\n" + e.message);
+      if (!posts) return res.sendStatus(404);
+      return res.status(200).send(posts);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
-router.delete("/:postId", async function (req, res) {
-  const deletedPost = await models.post.findByPk(req.params.postId);
-  await models.post.destroy({
-    where: { id: req.params.postId },
-  });
-  if (deletedPost.getDataValue("postId"))
-    await models.post.decrement("numComments", {
-      where: { id: deletedPost.getDataValue("postId") },
-    });
+router.get(
+  "/posts/:postId",
+  [authenticate, postExists],
+  async function (req, res, next) {
+    try {
+      const post = await models.post.findByPk(req.params.postId, {
+        include: [
+          {
+            model: models.post,
+            as: "comments",
+            include: [
+              {
+                model: models.user,
+                attributes: ["id", "firstName", "lastName", "imagePath"],
+              },
+              {
+                model: models.userPostLike,
+                include: {
+                  model: models.user,
+                  attributes: ["firstName", "lastName"],
+                },
+              },
+            ],
+            attributes: { exclude: ["userId", "postId"] },
+          },
+          {
+            model: models.user,
+            attributes: ["id", "firstName", "lastName", "imagePath"],
+          },
+          {
+            model: models.userPostLike,
+            include: {
+              model: models.user,
+              attributes: ["firstName", "lastName"],
+            },
+          },
+        ],
+        attributes: { exclude: ["postId"] },
+        order: [[Sequelize.col("comments.created_at"), "DESC"]],
+      });
+      if (
+        post.getDataValue("private") &&
+        req.userId !== post.getDataValue("userId")
+      )
+        return res.status(403).send("Access denied.");
 
-  return res.status(200).send();
-});
+      res.status(200).send(post);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
-router.post("/:postId/:userId", async function (req, res) {
-  const newLike = await models.userPostLike.create({
-    postId: req.params.postId,
-    userId: req.params.userId,
-  });
+router.post(
+  "/posts",
+  [authenticate, multer.single("image")],
+  async function (req, res, next) {
+    try {
+      const newPost = await models.post.create({
+        body: req.body.body,
+        private: false || req.body.private,
+        userId: req.userId,
+        imagePath: req.file ? req.file.originalname : null,
+      });
+      const newFullPost = await models.post.findByPk(newPost.id, {
+        include: [
+          {
+            model: models.user,
+            attributes: ["id", "firstName", "lastName", "imagePath"],
+          },
+        ],
+        attributes: { exclude: ["postId"] },
+      });
+      newFullPost.setDataValue("comments", []);
+      newFullPost.setDataValue("userPostLikes", []);
 
-  return res.status(200).send();
-});
+      if (req.file)
+        fs.writeFile(
+          "C:\\College\\Zavrsni\\Projekt\\client\\public\\img\\" +
+            req.file.originalname,
+          req.file.buffer
+        );
 
-router.delete("/:postId/:userId", async function (req, res) {
-  const newLike = await models.userPostLike.destroy({
-    where: { postId: req.params.postId, userId: req.params.userId },
-  });
+      res.status(201).send(newFullPost);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
-  return res.status(200).send();
-});
+router.put(
+  "/posts/:postId",
+  [authenticate, postExists],
+  async function (req, res, next) {
+    try {
+      const postId = req.params.postId;
+
+      const userOwnsPost =
+        (await models.post.findByPk(postId)).userId === req.userId;
+      if (!userOwnsPost) return res.status(403).send("Access denied.");
+
+      await models.post.update(
+        { body: req.body.body },
+        { where: { id: postId } }
+      );
+      const updatedPost = await models.post.findByPk(postId);
+
+      res.status(200).send(updatedPost);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.delete(
+  "/posts/:postId",
+  [authenticate, postExists],
+  async function (req, res, next) {
+    try {
+      const postId = req.params.postId;
+
+      const userOwnsPost =
+        (await models.post.findByPk(postId)).userId === req.userId;
+      if (!userOwnsPost) return res.status(403).send("Access denied.");
+
+      await models.post.destroy({ where: { id: postId } });
+
+      res.status(200).send();
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 module.exports = router;
--- server/routes/api/routes.js
+++ server/routes/api/routes.js
@@ -1,17 +1,19 @@
 var indexRouter = require("./index");
 var usersRouter = require("./users");
 var authRouter = require("./auth");
-var postRouter = require("./posts");
-var messageRouter = require("./messages");
-var chatRouter = require("./chats");
+var postsRouter = require("./posts");
+var messagesRouter = require("./messages");
+var chatsRouter = require("./chats");
 var friendsRouter = require("./friends");
+var friendRequestsRouter = require("./friendRequests");
 
 module.exports = (app) => {
   app.use("/", indexRouter);
   app.use("/api/users", usersRouter);
   app.use("/api/auth", authRouter);
-  app.use("/api/posts", postRouter);
-  app.use("/api/messages", messageRouter);
-  app.use("/api/", chatRouter);
+  app.use("/api", postsRouter);
+  app.use("/api/messages", messagesRouter);
+  app.use("/api/", chatsRouter);
   app.use("/api", friendsRouter);
+  app.use("/api", friendRequestsRouter);
 };
